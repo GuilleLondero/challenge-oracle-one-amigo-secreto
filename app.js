4e00e989afc8cae9f164d647c0588c4625f432b9
Guillermo Londero <guillelondero@gmail.com>
2025-02-17 20:14:28 -0300

    Commit inicial del challenge Amigo Secreto

---
+++ app.js
@@ -0,0 +1,21 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+
+/*                 INICIO
+                     \/
+ TOMO EL VALOR AGREGADO EN CAMPO DE TEXTO (IMPUT).
+                     \/        
+ DESARROLLO DE EVENTO EN BOTON AÑADIR. -> CONDICION DE VALIDACION DATO  ENTRADA, F-> MENSAJE ALERT ; 
+                                                                                 V->SE AGREGA NOMBRE A LA LISTA AMIGOS Y MUESTRO EN PANTALLA.
+                                                                                                            \/
+                                                                                                EVENTO BOTON CLICK SORTEAR
+                                                                                                            \/ 
+                                                                                           MUESTRO RESULTADO AMIGO SECRETO ALEATORIO
+                                                                                                            \/
+                                                                                                           FIN */              
+
+//DEFINICION DE VARIABLES Y ASIGNACION INICIAL:  
+
+    let listaAmigos = [];
+    
