6aca4f42ed5412577578848336b85c393430186d
Guillermo Londero <guillelondero@gmail.com>
2025-02-17 23:29:18 -0300

    agregue funcion amigos

--- app.js
+++ app.js
@@ -6,8 +6,8 @@
                      \/
  TOMO EL VALOR AGREGADO EN CAMPO DE TEXTO (IMPUT).
                      \/        
- DESARROLLO DE EVENTO EN BOTON AÑADIR. -> CONDICION DE VALIDACION DATO  ENTRADA, F-> MENSAJE ALERT ; 
-                                                                                 V->SE AGREGA NOMBRE A LA LISTA AMIGOS Y MUESTRO EN PANTALLA.
+ DESARROLLO DE EVENTO EN BOTON AÑADIR. -> CONDICION DE VALIDACION DATO  ENTRADA, v-> MENSAJE ALERT ; 
+                                                                                 F->SE AGREGA NOMBRE A LA LISTA AMIGOS Y MUESTRO EN PANTALLA.
                                                                                                             \/
                                                                                                 EVENTO BOTON CLICK SORTEAR
                                                                                                             \/ 
@@ -18,4 +18,36 @@
 //DEFINICION DE VARIABLES Y ASIGNACION INICIAL:  
 
     let listaAmigos = [];
+
+// IMPUT CAMPO DE TEXTO PARA AGREGAR UN AMIGO:
+    //primero debo vincular el campo HTML con js:
     
+    function agregarAmigos() 
+    {
+    let amigoUsuario = document.getElementById("amigo").value;
+    //ahora valido la entrada para que solo tome letras y impida el campo vacio:
+    if (amigoUsuario === "")
+        {
+            alert("Por favor debe ingresar un nombre! ");
+        }
+        else if ((!/^[a-zA-Z]+$/.test(amigoUsuario)))
+        {
+            alert("El nombre ingresado debe contener solo letras.");
+        }
+        else //agrego nombre a la lista 
+        {
+            listaAmigos.push(amigoUsuario);
+            limpiarCaja(); //Llamo a la función limpiarCaja() aquí
+        }
+console.log(amigoUsuario);
+console.log(listaAmigos);
+        return;
+    }
+
+    function limpiarCaja() // --Creo ahora una función para limpiar la caja de texto:
+    {
+    document.querySelector("#amigo").value = "";//let valorCaja = document.querySelector("#amigo"); y valorCaja.value = '';
+    }   
+   
+
+
